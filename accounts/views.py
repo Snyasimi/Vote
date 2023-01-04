1b9e9eb56791ddbbd5abd0632f3155bfe6c127ec
Snyasimi <nyasimisolo@gmail.com>
2023-01-04 12:57:07 +0300

    Fixed bug on the view form not saving data to the model

--- accounts/views.py
+++ accounts/views.py
@@ -9,18 +9,7 @@ def register(request):
         form = MyUserCreationForm(request.POST)
         
         if form.is_valid():
-            if form.cleaned_data['password1'] == form.cleaned_data['password2']:
-
-                new_user = MyUsers.objects.create_user(
-
-                        form.cleaned_data['Role_number'],
-                        form.cleaned_data["Name"],
-                        form.cleaned_data['Email'],
-                        form.cleaned_data["Dept_id"],
-                        password2
-
-                        )
-                new_user.save()
+            form.save()
 
 
 
