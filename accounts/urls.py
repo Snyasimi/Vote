2e3cae809af04e1fa80fdf1238cbd93c14f0fee7
S.Nyasimi <89997376+Snyasimi@users.noreply.github.com>
2023-01-09 22:41:10 +0300

    Reconfigured urls to match views

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,9 +3,8 @@ from . import views
 
 app_name="accounts"
 urlpatterns = [
-        path("",views.register,name="register"),
-        path("register-candidates",views.Register_candidate,name = "Reg_candidate"),
+        path("",views.CreateUser.as_view(),name="register"),
+        path("register-candidates",views.CreateCandidate.as_view(),name = "Reg_candidate"),
         path("View-candidates",views.CandidateView.as_view(),name="view-candidates"),
-
         ]
 
